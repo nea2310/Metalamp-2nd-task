e9638179932020209529744b61fb1a6da18d96d2
ekaterina <1988snea@gmail.com>
2021-06-15 12:38:59 +0400

    component calendar: saving dates in the local storage

--- src/components/date/date.js
+++ src/components/date/date.js
@@ -1,8 +1,8 @@
 class DatePicker {
 	constructor(root) {
 		this.wrapper = document.querySelector(root);
-		this.inputLeft = this.wrapper.querySelector('.date__input-left');
-		this.inputRight = this.wrapper.querySelector('.date__input-right');
+		this.inputDateFrom = this.wrapper.querySelector('.date__input-from');
+		this.inputDateTo = this.wrapper.querySelector('.date__input-to');
 		this.clWrapper = this.wrapper.querySelector('.date__calendarWrapper');
 		this.datePick();
 		this.datePrint();
@@ -16,10 +16,10 @@ class DatePicker {
 		this.clWrapper.addEventListener('click', (e) => {
 			if (e.target.classList.contains('-selected-')) {
 				console.log('SELECTED');
-				if (this.inputRight.value) {
+				if (this.inputDateTo.value) {
 					let dateArr = [];
-					let a = this.inputLeft.value.split('.');
-					let b = this.inputRight.value.split('.');
+					let a = this.inputDateFrom.value.split('.');
+					let b = this.inputDateTo.value.split('.');
 
 					dateArr.push(a[2] + '-' + a[1] + '-' + a[0]);
 					dateArr.push(b[2] + '-' + b[1] + '-' + b[0]);
@@ -45,8 +45,8 @@ class DatePicker {
 				}
 				if (dateArr.length == 1) {//если выбрана одиночная дата
 					if (datePrevArr == undefined) {//первичный выбор первой даты (вторая еще не выбиралась)
-						this.inputLeft.value = dateArr[0];
-						this.inputRight.value = '';
+						this.inputDateFrom.value = dateArr[0];
+						this.inputDateTo.value = '';
 					} else {/*очередной выбор даты, когда уже есть выбранный диапазон
 						(в соотв. с дефолтными настройками календаря, при этом диапазон деактивируется и активируется одиночная вновь выбранная дата)
 						*/
@@ -57,32 +57,33 @@ class DatePicker {
 
 						let currentDate =
 							new Date(a[2] + '-' + a[1] + '-' + a[0]);
-						let prevDateLeft =
+						let prevDateFrom =
 							new Date(b[2] + '-' + b[1] + '-' + b[0]);
-						let prevDateRight =
+						let prevDateTo =
 							new Date(c[2] + '-' + c[1] + '-' + c[0]);
 
-						if (currentDate < prevDateLeft) {//Новая дата раньше обеих старых дат
+						if (currentDate < prevDateFrom) {//Новая дата раньше обеих старых дат
 							this.myDatepicker.selectDate(currentDate);
-							this.myDatepicker.selectDate(prevDateLeft);
+							this.myDatepicker.selectDate(prevDateFrom);
 						}
 						else if
-							(currentDate > prevDateLeft &&
-							currentDate < prevDateRight) {//Новая дата между старыми датами
+							(currentDate > prevDateFrom &&
+							currentDate < prevDateTo) {//Новая дата между старыми датами
 							this.myDatepicker.clear();
-							this.myDatepicker.selectDate(prevDateLeft);
+							this.myDatepicker.selectDate(prevDateFrom);
 							this.myDatepicker.selectDate(currentDate);
 						}
-						else if (currentDate > prevDateRight) {//Новая дата позже обеих старых дат
+						else if (currentDate > prevDateTo) {//Новая дата позже обеих старых дат
 							this.myDatepicker.clear();
-							this.myDatepicker.selectDate(prevDateRight);
+							this.myDatepicker.selectDate(prevDateTo);
 							this.myDatepicker.selectDate(currentDate);
 						}
 					}
 				} else {//первичный выбор второй даты (первая дата уже выбрана)
-					this.inputLeft.value = dateArr[0];
-					this.inputRight.value = dateArr[1];
+					this.inputDateFrom.value = dateArr[0];
+					this.inputDateTo.value = dateArr[1];
 				}
+				this._updateLocalStorage();
 			}
 		}).data('datepicker');
 	}
@@ -91,13 +92,14 @@ class DatePicker {
 	datePrint() {
 		this.wrapper.addEventListener('change', (e) => {
 
-			if (e.target == this.inputLeft || e.target == this.inputRight) {
+			if (e.target == this.inputDateFrom ||
+				e.target == this.inputDateTo) {
 				let dateArr = [];
 				if (e.target.value) { //Если введена дата
 					this.currentInput = e.target;
-					this.currentInput.classList.contains('date__input-left') ?
-						this.secondInput = this.inputRight :
-						this.secondInput = this.inputLeft;
+					this.currentInput.classList.contains('date__input-from') ?
+						this.secondInput = this.inputDateTo :
+						this.secondInput = this.inputDateFrom;
 					let a = this.currentInput.value.split('.');
 					let b = this.secondInput.value.split('.');
 
@@ -112,8 +114,8 @@ class DatePicker {
 						this.myDatepicker.selectDate(new Date(dateArr[0]));
 					}
 				} else {//Если инпут очищен
-					if (e.target == this.inputLeft) { // очищен левый инпут
-						this.inputRight.value = '';// очистить правый инпут
+					if (e.target == this.inputDateFrom) { // очищен левый инпут
+						this.inputDateTo.value = '';// очистить правый инпут
 						this.myDatepicker.clear();// очистить календарь (снять выделение с обеих дат)
 					} else {// очищен правый инпут
 						let a = this.myDatepicker.selectedDates[1];
@@ -122,9 +124,21 @@ class DatePicker {
 				}
 			}
 		});
+		this._updateLocalStorage();
+
+	}
+
+
+	_updateLocalStorage() {
+		localStorage.setItem('dateFrom',
+			JSON.stringify(this.inputDateFrom.value));
+		localStorage.setItem('dateTo',
+			JSON.stringify(this.inputDateTo.value));
+		console.log(localStorage);
 	}
 }
 
 new DatePicker('#date__wrapper');
 
 
+console.log(localStorage);
