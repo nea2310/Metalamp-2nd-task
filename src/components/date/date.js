17073545f98ba295e8ad22ff0fcc2a05b68954a7
ekaterina <1988snea@gmail.com>
2021-06-18 23:22:42 +0400

    component calendar: styles

--- src/components/date/date.js
+++ src/components/date/date.js
@@ -11,6 +11,8 @@ class DatePicker {
 	}
 
 
+
+
 	/*Запретить деактивацию ранее выбранной даты при повторном клике на нее же*/
 	preventDateReset() {
 		this.clWrapper.addEventListener('click', (e) => {
@@ -34,9 +36,23 @@ class DatePicker {
 	datePick() {
 		let datePrevArr;
 		this.myDatepicker = $(this.clWrapper).datepicker({
+			clearButton: true,
+			navTitles: {
+				days: 'MM <i>yyyy</i>',
+			},
 			minDate: new Date(),
 			range: true,
 			multipleDates: true,
+			prevHtml: `<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 
+			17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 
+			8.01562H16.1755Z" fill="#BC9CFF"/>
+			</svg>`,
+			nextHtml: `<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 
+			15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676
+			 2.39062L8.36301 0.984375Z" fill="#BC9CFF"/>
+			</svg>`,
 			onSelect: (selectedDate) => {
 				let dateArr = selectedDate.split(',');
 				if (dateArr.length == 2) {// если выбран диапазон
