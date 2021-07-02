3c11d3a1b61a6a58e29d491c5ecb7b76d529425d
ekaterina <1988snea@gmail.com>
2021-07-02 17:14:29 +0400

    component Input: code for e-mail validation is changed (now is based on a class)

--- src/components/input-subscription/input-subscription.js
+++ src/components/input-subscription/input-subscription.js
@@ -1,21 +1,35 @@
+class InputSubscribe {
+	constructor(elemName, elem) {
+		this.elemName = elemName;
+		this.input = elem;
+		this.inputWrapper = this.input.parentNode;
+		this.link = this.inputWrapper.querySelector('button');
+		this.init();
 
-let inputsSubscribe = document.querySelectorAll('.js-check-email');
-function testEmail(value) {
-	let test = /.+@.+\..+/i.test(value);
-	if (value && !test) {
-		alert(`Введенный e-mail ${value} имеет некорректный формат`);
+	}
+	init() {
+		function testEmail(value) {
+			let test = /.+@.+\..+/i.test(value);
+			if (value && !test) {
+				alert(`Введенный e-mail ${value} имеет некорректный формат`);
+			}
+		}
+		this.input.addEventListener('change', () => {
+			testEmail(this.input.value);
+		});
+		this.link.addEventListener('click', () => {
+			testEmail(this.input.value);
+		});
 	}
 }
-for (let input of inputsSubscribe) {
-	let inputWrapper = input.parentNode;
-	let link = inputWrapper.querySelector('button');
-	input.addEventListener('change', () => {
-		testEmail(input.value);
-	});
-	link.addEventListener('click', () => {
-		testEmail(input.value);
-	});
+
+function inputsSubscribe() {
+	let inputsSubscribe = document.querySelectorAll('.js-check-email');
+	for (let inputSubscribe of inputsSubscribe) {
+		new InputSubscribe('js-check-email', inputSubscribe);
+	}
 }
+inputsSubscribe();
 
 
 
