bcf3abc50ebdbcfab0000238e7f9a06f5411a934
ekaterina <1988snea@gmail.com>
2021-07-12 16:28:12 +0400

    component Header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,9 +8,33 @@ class Header {
 		return wrapper.
 			querySelector('.' + this.elemName + '__' + selector);
 	}
+
+	getElems(selectors) {
+		let sel = '';
+		for (let selector of selectors) {
+			sel += '.' + this.elemName + '__' + selector + ',';
+		}
+		sel = sel.substring(0, sel.length - 1);
+		return this.wrapper.
+			querySelectorAll(sel);
+	}
 	render() {
 		this.burger = this.getElem('burger');
-		this.nav = this.getElem('nav');
+		this.navLevel1 = this.getElem('nav-level1');
+		this.navLevel2 = this.getElems(['nav-level2']);
+		this.tips = this.getElems(['nav-level1-item-img']);
+
+		this.navLevel1.addEventListener('mouseover', (e) => {
+			this.toggleLevel2Menu(e.relatedTarget);
+			this.toggleLevel2Menu(e.target);
+		});
+
+		this.tips.forEach(element => {
+			element.addEventListener('click', () => {
+				this.openCloseLevel2Menu(element);
+			});
+		});
+
 		this.burger.addEventListener('click', (e) => {
 			this.toggleMobileMenu();
 		});
@@ -23,13 +47,38 @@ class Header {
 
 	}
 
+	// показать/ скрыть меню второго уровня
+	toggleLevel2Menu(elem) {
+		if (elem.
+			matches(`.${this.elemName}__nav-level1-item-link`) &&
+			elem.nextElementSibling != null &&
+			elem.nextElementSibling.
+				matches(`.${this.elemName}__nav-level1-item-img`)
+		) {
+			for (let item of this.navLevel2) {
+				item.classList.remove('expanded');
+			}
+			elem.parentElement.
+				lastElementChild.classList.add('expanded');
+		}
+	}
+	// показать/ скрыть меню второго уровня
+	openCloseLevel2Menu(elem) {
+		if (elem.nextElementSibling.matches('.expanded')) {
+
+			elem.nextElementSibling.classList.remove('expanded');
+		} else {
+			for (let item of this.navLevel2) {
+				item.classList.remove('expanded');
+			}
+			elem.nextElementSibling.classList.add('expanded');
+		}
+	}
+	// показать/ скрыть мобильное меню
 	toggleMobileMenu() {
 		this.burger.classList.toggle('active');
 		this.nav.classList.toggle('active-nav');
 	}
-
-
-
 }
 
 function renderHeaders(selector) {
