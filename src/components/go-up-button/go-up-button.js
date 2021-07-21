e898b485e81894194b03b6f98beb07191eb5967e
ekaterina <1988snea@gmail.com>
2021-07-21 17:42:22 +0400

    component Go-Up Button: method to check the scroll and hide/show the button respectively

--- src/components/go-up-button/go-up-button.js
+++ src/components/go-up-button/go-up-button.js
@@ -4,11 +4,22 @@ class GoUpButton {
 		this.elemName = elemName.replace(/^./, '');
 		this.wrapper = elem;
 		this.click();
+		this.checkScroll();
 	}
 
+
+	checkScroll() {
+		window.addEventListener('scroll', () => {
+			if (window.pageYOffset >= 300) {
+				this.wrapper.classList.remove(`${this.elemName}_hidden`);
+			} else {
+				this.wrapper.classList.add(`${this.elemName}_hidden`);
+			}
+		});
+	}
 	// клик по кнопке
 	click() {
-		this.wrapper.addEventListener("click", () => {
+		this.wrapper.addEventListener('click', () => {
 			window.scrollTo(0, 0);
 		});
 	}
