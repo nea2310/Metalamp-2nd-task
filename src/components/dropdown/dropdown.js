ee2ec7407de1ae9e881df632b4714a1df817cc45
Ekaterina <1988snea@gmail.com>
2022-04-02 21:50:34 +0500

    dropdown.js fixed in line with ESLint FSD plugin rules and airbnb styleguide

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -4,6 +4,17 @@ class DropDown {
   constructor(elemName, elem) {
     this.elemName = elemName.replace(/^.js-/, '');
     this.wrapper = elem;
+    this.handleChangeCounter = this.handleChangeCounter.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleInsideClick = this.handleInsideClick.bind(this);
+    this.handleCollapseByClick = this.handleCollapseByClick.bind(this);
+    this.handleListFocus = this.handleListFocus.bind(this);
+    this.handleCollapseByFocus = this.handleCollapseByFocus.bind(this);
+    this.handleApply = this.handleApply.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+
     this.render();
     this.clickInput();
     this.focusInput();
@@ -23,9 +34,7 @@ class DropDown {
 
   getElems(selectors) {
     let sel = '';
-    for (const selector of selectors) {
-      sel += `.js-${this.elemName}__${selector},`;
-    }
+    selectors.forEach((selector) => { sel += `.js-${this.elemName}__${selector},`; });
     sel = sel.substring(0, sel.length - 1);
     return this.wrapper
       .querySelectorAll(sel);
@@ -46,16 +55,16 @@ class DropDown {
     this.btnsMinus = this.getElems(['count-decrem']);
     this.tip = this.getElem('img');
 
-    this.btnClear != null && this.btnApply != null
-      ? this.clearApplyBtns = true
-      : this.clearApplyBtns = false;
+    // this.clearApplyBtns = !!(this.btnClear != null && this.btnApply != null);
+    this.clearApplyBtns = this.btnClear != null && this.btnApply != null;
     this.getInitialCounterList(this.listElems);
   }
 
-  /* Получение начального состояния счетчиков (текущее значение; является ли оно минимальным или максимальным; название и тип категории) */
+  /* Получение начального состояния счетчиков (текущее значение;
+    является ли оно минимальным или максимальным; название и тип категории) */
   getInitialCounterList(counterList) {
     this.counters = [];
-    for (let i = 0; i < counterList.length; i++) {
+    for (let i = 0; i < counterList.length; i += 1) {
       const elemObj = {};
       const catName = this.getElem('cat', counterList[i]);
       const catCnt = this.getElem('count-val', counterList[i]);
@@ -69,13 +78,8 @@ class DropDown {
       elemObj.maxCnt = catIncrem.getAttribute('data-max');
       elemObj.minCnt = catDecrem.getAttribute('data-min');
 
-      elemObj.cnt == elemObj.maxCnt
-        ? elemObj.isMax = true
-        : elemObj.isMax = false;
-
-      elemObj.cnt == elemObj.minCnt
-        ? elemObj.isMin = true
-        : elemObj.isMin = false;
+      elemObj.isMax = elemObj.cnt === elemObj.maxCnt;
+      elemObj.minCnt = elemObj.cnt === elemObj.minCnt;
 
       this.counters.push(elemObj);
     }
@@ -86,9 +90,10 @@ class DropDown {
     }
   }
 
-  /* определяем неактивные кнопки (если начальное значение счетчика - минимальное или максимальное) */
+  /* определяем неактивные кнопки (если начальное значение счетчика -
+    минимальное или максимальное) */
   initializeButtons(counterList) {
-    for (let i = 0; i < counterList.length; i++) {
+    for (let i = 0; i < counterList.length; i += 1) {
       const elem = this.listElems[i];
       if (counterList[i].isMin) {
         const minus = this.getElem('count-decrem', elem);
@@ -101,39 +106,93 @@ class DropDown {
     }
   }
 
+  handleChangeCounter(e) {
+    const elem = e.currentTarget;
+    const text = e.target.parentElement.parentElement
+      .firstElementChild.innerText.toLowerCase();
+    let editedCounter;
+    // Для кнопки "минус"
+    if (elem.classList
+      .contains(`${this.elemName}__count-decrem`)) {
+      // Сделать активной кнопку "плюс" при клике на кнопку "минус"
+      elem.parentElement.lastElementChild.disabled = false;
+      // Уменьшить счетчик на единицу
+      const currentCounter = parseInt(e.target.nextElementSibling.innerText, 10);
+      editedCounter = String(currentCounter - 1);
+      e.target.nextElementSibling.innerText = editedCounter;
+    } else { // для кнопки "плюс": Сделать активной кнопку "минус" при клике на кнопку "плюс"
+      elem.parentElement.firstElementChild.disabled = false;
+      if (this.clearApplyBtns) { // Показать кнопку [Очистить]
+        this.btnClear
+          .classList.remove(`${this.elemName}__button_hidden`);
+      }
+      // Увеличить счетчик на единицу
+      const currentCounter = parseInt(e.target.previousElementSibling.innerText, 10);
+      editedCounter = String(currentCounter + 1);
+      e.target.previousElementSibling.innerText = editedCounter;
+    }
+    this.updateCounterList(text, editedCounter);
+  }
+
+  handleMouseDown() {
+    this.mouseDown = true;
+  }
+
+  handleMouseUp() {
+    this.toggle(true);
+    this.mouseDown = false;
+  }
+
+  handleFocus() {
+    if (this.listWrapper.classList
+      .contains(`${this.elemName}__list-wrapper_hidden`)
+      && this.mouseDown === false) {
+      this.toggle(true);
+    }
+  }
+
+  handleInsideClick() {
+    this.clickOnList = true;
+  }
+
+  handleCollapseByClick() {
+    if (this.clickOnList === false) {
+      this.toggle(false);
+    } else {
+      this.clickOnList = false;
+    }
+  }
+
+  handleListFocus() {
+    this.focusOnList = true;
+  }
+
+  handleCollapseByFocus() {
+    if (this.focusOnList === false) {
+      this.toggle(false);
+    } else {
+      this.focusOnList = false;
+    }
+  }
+
+  handleApply() {
+    this.toggle(true);
+  }
+
+  handleClear() {
+    for (let i = 0; i < this.countVals.length; i += 1) {
+      this.countVals[i].innerText = this.counters[i].minCnt;
+      this.updateCounterList(
+        this.counters[i].text,
+        this.countVals[i].innerText,
+      );
+    }
+    this.input.value = '';
+  }
+
   // обработка клика по кнопкам Плюс / Минус
   changeCounter() {
-    for (const elem of this.counts) {
-      elem.addEventListener('click', (e) => {
-        const text = e.target.parentElement.parentElement
-          .firstElementChild.innerText.toLowerCase();
-        let editedCounter;
-        // Для кнопки "минус"
-        if (elem.classList
-          .contains(`${this.elemName}__count-decrem`)) {
-          // Сделать активной кнопку "плюс" при клике на кнопку "минус"
-          elem.parentElement.lastElementChild.disabled = false;
-          // Уменьшить счетчик на единицу
-          const currentCounter = parseInt(e.target.nextElementSibling.innerText);
-          editedCounter = String(currentCounter - 1);
-          e.target.nextElementSibling.innerText = editedCounter;
-        }
-        // Для кнопки "плюс"
-        else {
-          // Сделать активной кнопку "минус" при клике на кнопку "плюс"
-          elem.parentElement.firstElementChild.disabled = false;
-          if (this.clearApplyBtns) { // Показать кнопку [Очистить]
-            this.btnClear
-              .classList.remove(`${this.elemName}__button_hidden`);
-          }
-          // Увеличить счетчик на единицу
-          const currentCounter = parseInt(e.target.previousElementSibling.innerText);
-          editedCounter = String(currentCounter + 1);
-          e.target.previousElementSibling.innerText = editedCounter;
-        }
-        this.updateCounterList(text, editedCounter);
-      });
-    }
+    this.counts.forEach((elem) => elem.addEventListener('click', this.handleChangeCounter));
   }
 
   /* обновление состояния счетчиков */
@@ -166,18 +225,21 @@ class DropDown {
     this.updateCategoriesList(this.counters);
   }
 
-  /* обновление кнопок Плюс/ Минус (делаем неактивными, если достигнуто минимальное/ максимальное значение) */
+  /* обновление кнопок Плюс/ Минус (делаем неактивными,
+    если достигнуто минимальное/ максимальное значение) */
   updateButtons(counters) {
-    for (let i = 0; i < counters.length; i++) {
+    for (let i = 0; i < counters.length; i += 1) {
       const { cnt } = counters[i];
       const cntToChange = this.listElems[i]
         .querySelector(`.${this.elemName}__count-val`);
       cntToChange.innerText = cnt;
-      // Если обновленное значение - минимальное разрешенное значение, то сделать кнопку "минус" неактивной
+      /* Если обновленное значение - минимальное разрешенное значение,
+      то сделать кнопку "минус" неактивной */
       if (counters[i].isMin) {
         cntToChange.previousElementSibling.disabled = true;
       }
-      // Если обновленное значение - максимальное разрешенное значение, то сделать кнопку "плюс" неактивной
+      /* Если обновленное значение - максимальное разрешенное значение,
+      то сделать кнопку "плюс" неактивной */
       if (counters[i].isMax) {
         cntToChange.nextElementSibling.disabled = true;
       }
@@ -190,14 +252,12 @@ class DropDown {
   // Скрыть кнопку [очистить]
   hideBtnClear(btnsMinus) {
     const arr = [];
-    for (const btn of btnsMinus) {
-      arr.push(btn.disabled);
-    }
+    btnsMinus.forEach((btn) => arr.push(btn.disabled));
     // есть ли среди кнопок "Минус" активные (disabled==false)
     let isCleared = arr.find((item) => item
-      == false);
+      === false);
     // если активные кнопки не обнаружены - isCleared=true
-    if (isCleared == undefined) {
+    if (isCleared === undefined) {
       isCleared = true;
       // скрыть кнопку [Очистить]
       this.btnClear.classList.add(`${this.elemName}__button_hidden`);
@@ -207,7 +267,7 @@ class DropDown {
   /* Обновление списка категорий, которые выводятся в инпуте */
   updateCategoriesList(changedCounters) {
     this.countersToDisplay = [];
-    for (let i = 0; i < changedCounters.length; i++) {
+    for (let i = 0; i < changedCounters.length; i += 1) {
       // Если категории такого типа еще нет
       if (i == 0 || i > 0
         && changedCounters[i].type != changedCounters[i - 1].type) {
@@ -223,12 +283,12 @@ class DropDown {
       }
       // Если  категория такого типа уже есть
       if (i > 0 && changedCounters[i].type
-        == changedCounters[i - 1].type) {
+        === changedCounters[i - 1].type) {
         const elem = this.countersToDisplay.find((item) => item.type
-          == changedCounters[i].type);
+          === changedCounters[i].type);
         // То в массив не добавлять, а прибавить значение к значению счетчика этой категории
-        elem.cnt = String(parseInt(elem.cnt)
-          + parseInt(changedCounters[i].cnt));
+        elem.cnt = String(parseInt(elem.cnt, 10)
+          + parseInt(changedCounters[i].cnt, 10));
       }
     }
     this.updateInput(this.countersToDisplay);
@@ -237,20 +297,21 @@ class DropDown {
   /* обновление значения в инпуте */
   updateInput(countersToDisplay) {
     // Определение падежа категории в зависимости от значения счетчика
-    function getWordForm(value, words) {
+    function getWordForm(val, words) {
+      let value = val;
       value = Math.abs(value) % 100;
       const num = value % 10;
       if (value > 10 && value < 20) return words[2];
       if (num > 1 && num < 5) return words[1];
-      if (num == 1) return words[0];
+      if (num === 1) return words[0];
       return words[2];
     }
     let value = '';
     countersToDisplay.forEach((counter) => {
       // исключаем категории, у которых счетчик = 0
-      if (parseInt(counter.cnt) != 0) {
+      if (parseInt(counter.cnt, 10) !== 0) {
         value += `${counter.cnt} ${getWordForm(
-          parseInt(counter.cnt),
+          parseInt(counter.cnt, 10),
           counter.typeforms,
         )}, `;
       }
@@ -280,93 +341,52 @@ class DropDown {
 
   // клик по инпуту
   clickInput() {
-    this.input.addEventListener('mousedown', () => {
-      this.mouseDown = true;
-    });
-
-    this.input.addEventListener('mouseup', () => {
-      this.toggle(true);
-      this.mouseDown = false;
-    });
+    this.input.addEventListener('mousedown', this.handleMouseDown);
+    this.input.addEventListener('mouseup', this.handleMouseUp);
   }
 
   // фокус на инпут
   focusInput() {
-    this.input.addEventListener('focus', () => {
-      if (this.listWrapper.classList
-        .contains(`${this.elemName}__list-wrapper_hidden`)
-        && this.mouseDown == false) {
-        this.toggle(true);
-      }
-    });
+    this.input.addEventListener('focus', this.handleFocus);
   }
 
   // проверка, клик был снаружи или внутри виджета
   insideListClick() {
-    this.wrapper.addEventListener('click', () => {
-      this.clickOnList = true;
-    });
+    this.wrapper.addEventListener('click', this.handleInsideClick);
   }
 
   // отлавливаем все клики по документу, если клик снаружи виджета - сворачиваем виджет
   collapseByClick() {
-    document.addEventListener('click', () => {
-      if (this.clickOnList == false) {
-        this.toggle(false);
-      } else {
-        this.clickOnList = false;
-      }
-    });
+    document.addEventListener('click', this.handleCollapseByClick);
   }
 
   // проверка, фокус был снаружи или внутри виджета
   insideListFocus() {
-    this.wrapper.addEventListener('focusin', () => {
-      this.focusOnList = true;
-    });
+    this.wrapper.addEventListener('focusin', this.handleListFocus);
   }
 
   // отлавливаем все фокусы по документу, если фокус снаружи виджета - сворачиваем виджет
   collapseByFocus() {
-    document.addEventListener('focusin', () => {
-      if (this.focusOnList == false) {
-        this.toggle(false);
-      } else {
-        this.focusOnList = false;
-      }
-    });
+    document.addEventListener('focusin', this.handleCollapseByFocus);
   }
 
   // клик по кнопке [Применить]
   apply() {
     if (this.clearApplyBtns) {
-      this.btnApply.addEventListener('click', () => {
-        this.toggle(true);
-      });
+      this.btnApply.addEventListener('click', this.handleApply);
     }
   }
 
   // клик по кнопке [очистить]
   clear() {
     if (this.clearApplyBtns) {
-      this.btnClear.addEventListener('click', () => {
-        for (let i = 0; i < this.countVals.length; i++) {
-          this.countVals[i].innerText = this.counters[i].minCnt;
-          this.updateCounterList(
-            this.counters[i].text,
-            this.countVals[i].innerText,
-          );
-        }
-        this.input.value = '';
-      });
+      this.btnClear.addEventListener('click', this.handleClear);
     }
   }
 }
 
 function renderDropDowns(selector) {
   const dropDowns = document.querySelectorAll(selector);
-  for (const dropDown of dropDowns) {
-    new DropDown(selector, dropDown);
-  }
+  dropDowns.forEach((dropDown) => new DropDown(selector, dropDown));
 }
 renderDropDowns('.js-dropdown');
