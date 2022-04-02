0976a67219f9cba183f5b931dab12a8bc28709c1
Ekaterina <1988snea@gmail.com>
2022-04-02 13:48:30 +0500

    booking.js fixed in line with ESLint FSD plugin rules

--- src/components/booking/booking.js
+++ src/components/booking/booking.js
@@ -1,5 +1,5 @@
-
 import './booking.scss';
+
 class Booking {
   constructor(elemName, elem) {
     this.elemName = elemName.replace(/^./, '');
@@ -12,52 +12,54 @@ class Booking {
   render() {
     this.dates = this.wrapper.querySelectorAll('.date-dropdown__input');
     this.guests = this.wrapper.querySelector('.dropdown__input');
-    this.inputs = this.wrapper.
-      querySelectorAll('.dropdown__input, .date-dropdown__input');
+    this.inputs = this.wrapper
+      .querySelectorAll('.dropdown__input, .date-dropdown__input');
+  }
+
+  handleFocusInput() { date.classList.remove('js-err'); }
+
+  handleFormSubmit() {
+    let isErr = false;
+    this.dates.forEach((date) => {
+      if (/^\d{2}\.\d{2}\.\d{4}$/.test(date.value)) {
+        date.classList.remove('js-err');
+      } else {
+        date.classList.add('js-err');
+      }
+    });
+    if (this.guests.value.trim() === '') {
+      this.guests.classList.add('js-err');
+    } else {
+      this.guests.classList.remove('js-err');
+    }
+    for (let i = 0; i < this.inputs.length; i++) {
+      if (this.inputs[i].classList.contains('js-err')) {
+        isErr = true;
+        break;
+      }
+    }
+    if (isErr) {
+      e.preventDefault();
+      alert('Заполните все поля!');
+    }
   }
 
   // При фокусе убрать красную рамку с инпута
   focusInput() {
-    this.inputs.forEach(function (date) {
-      date.addEventListener('focus', () => {
-        date.classList.remove('js-err');
-      });
+    this.inputs.forEach((date) => {
+      date.addEventListener('focus', this.handleFocusInput);
     });
   }
+
   // Валидация инпутов на сабмите формы
   formSubmit() {
-    this.wrapper.addEventListener('submit', (e) => {
-      let isErr = false;
-      this.dates.forEach(function (date) {
-        if (/^\d{2}\.\d{2}\.\d{4}$/.test(date.value)) {
-          date.classList.remove('js-err');
-        }
-        else {
-          date.classList.add('js-err');
-        }
-      });
-      if (this.guests.value.trim() === '') {
-        this.guests.classList.add('js-err');
-      } else {
-        this.guests.classList.remove('js-err');
-      }
-      for (let i = 0; i < this.inputs.length; i++) {
-        if (this.inputs[i].classList.contains('js-err')) {
-          isErr = true;
-          break;
-        }
-      }
-      if (isErr) {
-        e.preventDefault();
-        alert('Заполните все поля!');
-      }
-    });
+    this.wrapper.addEventListener('submit', this.handleFormSubmit);
   }
 }
 
 function renderBookings(selector) {
-  let bookings = document.querySelectorAll(selector);
-  for (let booking of bookings) {
+  const bookings = document.querySelectorAll(selector);
+  for (const booking of bookings) {
     new Booking(selector, booking);
   }
 }
