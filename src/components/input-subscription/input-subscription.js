a2e59e6f30479568d45b03393312e1275734bc29
Ekaterina <1988snea@gmail.com>
2022-04-06 19:19:36 +0500

    input-subscription.js fixed in line with ESLint FSD plugin rules and airbnb styleguide

--- src/components/input-subscription/input-subscription.js
+++ src/components/input-subscription/input-subscription.js
@@ -1,34 +1,44 @@
+/* eslint-disable no-alert */
 class InputSubscribe {
   constructor(elemName, elem) {
     this.elemName = elemName.replace(/^./, '');
     this.input = elem;
     this.inputWrapper = this.input.parentNode;
-    this.link = this.inputWrapper.querySelector('button');
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.render();
     this.init();
   }
 
-  init() {
-    function testEmail(value) {
-      const test = /.+@.+\..+/i.test(value);
-      if (value && !test) {
-        alert(`Введенный e-mail ${value} имеет некорректный формат`);
-      }
+  static testEmail(value) {
+    const test = /.+@.+\..+/i.test(value);
+    if (value && !test) {
+      alert(`Введенный e-mail ${value} имеет некорректный формат`);
     }
-    this.input.addEventListener('change', () => {
-      testEmail(this.input.value);
-    });
+  }
+
+  render() {
+    this.link = this.inputWrapper.querySelector('button');
+  }
+
+  handleChange() {
+    InputSubscribe.testEmail(this.input.value);
+  }
+
+  handleClick() {
+    InputSubscribe.testEmail(this.input.value);
+  }
+
+  init() {
+    this.input.addEventListener('change', this.handleChange);
     if (this.link) {
-      this.link.addEventListener('click', () => {
-        testEmail(this.input.value);
-      });
+      this.link.addEventListener('click', this.handleClick);
     }
   }
 }
 
-function inputsSubscribe(selector) {
+function renderInputsSubscribe(selector) {
   const inputsSubscribe = document.querySelectorAll(selector);
-  for (const inputSubscribe of inputsSubscribe) {
-    new InputSubscribe(selector, inputSubscribe);
-  }
+  inputsSubscribe.forEach((inputMask) => new InputSubscribe(selector, inputMask));
 }
-inputsSubscribe('.js-check-email');
+renderInputsSubscribe('.js-check-email');
