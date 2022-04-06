90b8a1841ee54ca801c65d5f64fdd546d5f6829d
Ekaterina <1988snea@gmail.com>
2022-04-06 19:27:49 +0500

    like-button.js fixed in line with ESLint FSD plugin rules and airbnb styleguide

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,9 +1,18 @@
 import './like-button.scss';
 
+const iconLike = require(
+  './img/favorite.svg',
+);
+const iconUnlike = require(
+  './img/favorite-border.svg',
+);
+
 class LikeButton {
   constructor(elemName, elem) {
     this.elemName = elemName.replace(/^.js-/, '');
     this.wrapper = elem;
+    this.handleClick = this.handleClick.bind(this);
+    this.handleLoad = this.handleLoad.bind(this);
     this.render();
     this.click();
     this.updLikeStatus();
@@ -18,50 +27,44 @@ class LikeButton {
     this.button = this.getElem('button');
     this.img = this.getElem('img');
     this.counter = this.getElem('counter');
+  }
 
-    this.iconLike = require(
-      './img/favorite.svg',
-    );
+  handleClick() {
+    const val = parseInt(this.counter.innerText, 10);
+    this.button.classList.toggle(`${this.elemName}_liked`);
+    if (this.button.classList.contains(`${this.elemName}_liked`)) {
+      this.img.src = iconLike;
+      this.counter.innerText = val + 1;
+      localStorage.setItem('isLiked', 'liked');
+    } else {
+      this.img.src = iconUnlike;
+      this.counter.innerText = val - 1;
+      localStorage.setItem('isLiked', 'unliked');
+    }
+  }
 
-    this.iconUnlike = require(
-      './img/favorite-border.svg',
-    );
+  handleLoad() {
+    if (localStorage.getItem('isLiked') === 'liked') {
+      this.button.classList.add(`${this.elemName}_liked`);
+      this.img.src = iconLike;
+    } else {
+      this.button.classList.remove(`${this.elemName}_liked`);
+      this.img.src = iconUnlike;
+    }
   }
 
   // клик по кнопке
   click() {
-    this.button.addEventListener('click', () => {
-      const val = parseInt(this.counter.innerText);
-      this.button.classList.toggle(`${this.elemName}_liked`);
-      if (this.button.classList.contains(`${this.elemName}_liked`)) {
-        this.img.src = this.iconLike;
-        this.counter.innerText = val + 1;
-        localStorage.setItem('isLiked', 'liked');
-      } else {
-        this.img.src = this.iconUnlike;
-        this.counter.innerText = val - 1;
-        localStorage.setItem('isLiked', 'unliked');
-      }
-    });
+    this.button.addEventListener('click', this.handleClick);
   }
 
   updLikeStatus() {
-    window.addEventListener('load', () => {
-      if (localStorage.getItem('isLiked') === 'liked') {
-        this.button.classList.add(`${this.elemName}_liked`);
-        this.img.src = this.iconLike;
-      } else {
-        this.button.classList.remove(`${this.elemName}_liked`);
-        this.img.src = this.iconUnlike;
-      }
-    });
+    window.addEventListener('load', this.handleLoad);
   }
 }
 
 function renderLikeButtons(selector) {
   const likeButtons = document.querySelectorAll(selector);
-  for (const likeButton of likeButtons) {
-    new LikeButton(selector, likeButton);
-  }
+  likeButtons.forEach((likeButton) => new LikeButton(selector, likeButton));
 }
 renderLikeButtons('.js-like-button');
