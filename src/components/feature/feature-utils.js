e49451618d458dbac59c5fdc581f4c808c8b75dc
Ekaterina <1988snea@gmail.com>
2022-07-28 13:02:48 +0500

    component 'feature': moved js-code from PUG-mixin to a separate file #23

---
+++ src/components/feature/feature-utils.js
@@ -0,0 +1,10 @@
+import isImage from '../../shared/helpers/isImage';
+
+export default function prepareOptions(options) {
+  const { imageName } = options;
+
+  return {
+    elementName: 'feature',
+    imagePathChecked: isImage(imageName),
+  };
+}
