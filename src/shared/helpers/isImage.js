47705b2e930a0778f6c9bd26d98a288c140041a9
Ekaterina <1988snea@gmail.com>
2022-07-28 14:37:19 +0500

    moved images used by more than one component to folder 'shared/images' #23

--- src/shared/helpers/isImage.js
+++ src/shared/helpers/isImage.js
@@ -2,5 +2,5 @@
 /* eslint-disable global-require */
 export default function isImage(path) {
   const result = /^assets\/images\//.test(path);
-  return result ? path : require('@com/feature/image/default.svg');
+  return result ? path : require('@/shared/images/logo-purple.svg');
 }
