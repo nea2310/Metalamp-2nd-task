666dd363b937c9c23dac795d5b8a8e259af4451b
Ekaterina <1988snea@gmail.com>
2022-07-29 22:00:16 +0500

    Code refactoring; removed useless helper 'isImage'

--- src/shared/helpers/isImage.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable global-require */
-export default function isImage(path) {
-  const result = /^assets\/images\//.test(path);
-  return result ? path : require('@/shared/images/logo-purple.svg');
-}
