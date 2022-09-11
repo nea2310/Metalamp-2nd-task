bfd64965b58a49ea758eb21a361531dfb21d4fe7
Ekaterina <1988snea@gmail.com>
2022-09-11 20:12:58 +0500

    Fixed Header logic #25

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,13 +3,14 @@ class Header {
     this.elementName = elementName.replace(/^.js-/, '');
     this.wrapper = element;
 
-    this._handleHeaderKeydownLevel1 = this._handleHeaderKeydownLevel1.bind(this);
-    this._handleHeaderClickLevel2 = this._handleHeaderClickLevel2.bind(this);
+    this._handleHeaderKeydownTip = this._handleHeaderKeydownTip.bind(this);
+    this._handleHeaderClickTip = this._handleHeaderClickTip.bind(this);
     this._handleHeaderClickBurger = this._handleHeaderClickBurger.bind(this);
     this._handleHeaderResizeWindow = this._handleHeaderResizeWindow.bind(this);
     this._handleHeaderClickDoc = this._handleHeaderClickDoc.bind(this);
     this._handleHeaderFocusinDoc = this._handleHeaderFocusinDoc.bind(this);
-    this._handleHeaderMouseoverLevel1 = this._handleHeaderMouseoverLevel1.bind(this);
+    this._handleHeaderFocusLinkLevel1 = this._handleHeaderFocusLinkLevel1.bind(this);
+    this._handleHeaderKeydownNavLevel2 = this._handleHeaderKeydownNavLevel2.bind(this);
 
     this._render();
     this._bindEventListeners();
@@ -20,23 +21,22 @@ class Header {
     this.content = this._getElement('content');
     this.navLevel1 = this._getElement('nav-level1');
     this.navLevel2 = this._getElements(['nav-level2']);
-    this.menuItemsLevel1 = this._getElements(['nav-level1-item']);
     this.tips = this._getElements(['nav-level1-item-tip']);
-    this.linksLevel1 = this._getElements(['__nav-level1-item-link']);
+    this.linksLevel1 = this._getElements(['nav-level1-item-link']);
   }
 
   _bindEventListeners() {
-    this.navLevel1.addEventListener('keydown', this._handleHeaderKeydownLevel1);
-    this.menuItemsLevel1.forEach((element) => {
-      element.addEventListener('mouseover', this._handleHeaderMouseoverLevel1);
+    this.linksLevel1.forEach((element) => {
+      element.addEventListener('focus', this._handleHeaderFocusLinkLevel1);
     });
 
-    this.linksLevel1.forEach((element) => {
-      element.addEventListener('keydown', this._handleHeaderKeydownLevel1);
+    this.navLevel2.forEach((element) => {
+      element.addEventListener('keydown', this._handleHeaderKeydownNavLevel2);
     });
 
     this.tips.forEach((element) => {
-      element.addEventListener('click', this._handleHeaderClickLevel2);
+      element.addEventListener('click', this._handleHeaderClickTip);
+      element.addEventListener('keydown', this._handleHeaderKeydownTip);
     });
     this.burger.addEventListener('click', this._handleHeaderClickBurger);
     document.addEventListener('click', this._handleHeaderClickDoc);
@@ -44,26 +44,32 @@ class Header {
     window.addEventListener('resize', this._handleHeaderResizeWindow);
   }
 
-  _handleHeaderMouseoverLevel1(e) {
-    const list = e.target.querySelector(`.${this.elementName}__nav-level2`);
-    if (list) {
-      list.classList.add(`${this.elementName}__nav-level2-item_expanded`);
+  _handleHeaderFocusLinkLevel1() {
+    this._closeLevel2Menu();
+  }
+
+  _handleHeaderKeydownNavLevel2(e) {
+    if (e.keyCode === 27) {
+      this._closeLevel2Menu();
+      e.currentTarget.parentElement.querySelector(`.${this.elementName}__nav-level1-item-tip`).focus();
     }
   }
 
-  _handleHeaderClickLevel2(e) {
+  _handleHeaderClickTip(e) {
     const list = e.currentTarget.parentElement.querySelector(`.${this.elementName}__nav-level2`);
     if (list) {
       list.classList.toggle(`${this.elementName}__nav-level2-item_expanded`);
     }
   }
 
-  _handleHeaderKeydownLevel1(e) {
+  _handleHeaderKeydownTip(e) {
     if (e.keyCode === 32) {
       e.preventDefault();
       const list = e.target.parentElement.querySelector(`.${this.elementName}__nav-level2`);
       if (list) {
         list.classList.toggle(`${this.elementName}__nav-level2-item_expanded`);
+        const firstItem = list.querySelector('.header__nav-level2-item-link');
+        firstItem.focus();
       }
     }
   }
