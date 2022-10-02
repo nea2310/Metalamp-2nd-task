7b7b33d3b2968a37757579db94e4407d127c5a8f
Ekaterina <1988snea@gmail.com>
2022-10-02 19:58:54 +0500

    Added right padding to input field if it has arrow button #25

--- src/components/input-field/input-field-utils.js
+++ src/components/input-field/input-field-utils.js
@@ -4,13 +4,18 @@ export default function prepareOptions(options) {
   const {
     width,
     validation,
+    hasArrow,
   } = options;
+
+  const elementName = 'input-field';
 
   const validWidths = ['wide', 'medium', 'medium-narrow'];
   const validationTypes = ['date', 'email'];
+  const arrowModifier = hasArrow ? ` ${elementName}__input_with-arrow` : '';
 
   return {
-    elementName: 'input-field',
+    elementName,
+    arrowModifier,
     widthChecked: getValidValue(validWidths, width, 'wide'),
     validationChecked: getValidValue(validationTypes, validation),
   };
